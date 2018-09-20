0d07dd89c4df24002b4af13c7e53a038c029fd03
luke marcucci <200372453@student.georgianc.on.ca>
2018-09-19 22:47:50 -0400

    lab 2

---
+++ ASP-fundamentals/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_fundamentals.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Details(string DetailsName)
+        {
+            ViewBag.DetailsName = DetailsName;
+            return View();
+        }
+    }
+}
